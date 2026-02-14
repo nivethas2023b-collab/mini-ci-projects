fedcdc62bcea8c53eb1ae436409befd5a3cb39f6
nivethas2023b-collab <nivetha.s2023b@vitstudent.ac.in>
2026-02-14 21:15:10 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Mini CI Project Running Successfully");
+        System.out.println("Mini CI Project Running Successfully")
     }
+
 }
